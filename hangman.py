693292e6edcdeaa2575ada3a62b597fcbcd0e45f
ViktorUshkov <mcderrigosh@yandex.ru>
2024-02-20 00:01:53 +0300

    background added

--- hangman.py
+++ hangman.py
@@ -10,9 +10,14 @@ class Hangman:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Hangman')
 
+        self.background = pygame.image.load('assets/background.jpg')
+        self.background = pygame.transform.scale(self.background,
+                                                 (self.settings.screen_width, self.settings.screen_height))
+
     def run_game(self):
         while True:
             self._check_events()
+            self._update_window()
             self.clock.tick(60)
 
     def _check_events(self):
@@ -20,3 +25,13 @@ class Hangman:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_position = pygame.mouse.get_pos()
+                self._check_play_button_click(mouse_position)
+
+    def _check_play_button_click(self, mouse_position):
+        pass
+
+    def _update_window(self):
+        self.screen.blit(self.background, (0, 0))
+        pygame.display.flip()
